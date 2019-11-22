e881edc7d4ad04c28252ec4d2040897852573d36
Ran Li <rot_j@yahoo.com>
2019-11-22 11:01:45 -0500

    Restore MetadataSearchResult

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -147,7 +147,7 @@ class AvalonXmlMovieAgent(Agent.Movies):
 
         # Plex throws exception that have "/" in ID
         mid = b64encode("%s:%d" % (title, year)).replace("/", "_")
-        results.Append(MetadataSearchResult(id=mid, name=title, year=year, lang="xn", score=100))
+        results.Append(MetadataSearchResult(id=mid, name=title, year=year, lang=lang, score=100))
         PlexLog.debug("====================  Search end  ====================")
 
     def update(self, metadata, media, lang, force):
